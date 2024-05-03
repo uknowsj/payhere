89a1eaf9a15b933441db518daabbbc1cf780a517
uknowsj <uknowsj@gmail.com>
2024-05-03 10:59:09 +0900

    🔧 SVG 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import path from 'path'
 
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-
+import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), svgr()],
 	resolve: {
 		alias: [
 			{ find: '@', replacement: path.resolve(__dirname, 'src') },
