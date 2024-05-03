10f493d6af49708fb3d77de901a121c1ad2b182d
uknowsj <uknowsj@gmail.com>
2024-05-03 10:30:44 +0900

    🔧 vite 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import path from 'path'
+
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{ find: '@', replacement: path.resolve(__dirname, 'src') },
+			{ find: '@/assets', replacement: path.resolve(__dirname, 'src/assets') },
+			{ find: '@/components', replacement: path.resolve(__dirname, 'src/components') },
+			{ find: '@/ui', replacement: path.resolve(__dirname, 'src/ui') },
+			{ find: '@/utils', replacement: path.resolve(__dirname, 'src/utils') },
+			{
+				find: 'class-variance-authority/types',
+				replacement: path.resolve(__dirname, './node_modules/class-variance-authority/dist/types.d.ts'),
+			},
+		],
+	},
 })
