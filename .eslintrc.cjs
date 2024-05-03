b6ec8040cae1035f7b9415877064dbc7b1c7313c
uknowsj <uknowsj@gmail.com>
2024-05-03 10:12:50 +0900

    🚧 ESLint, Prettier 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,28 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:prettier/recommended',
+		'plugin:tailwindcss/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'import'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'import/order': [
+			'warn',
+			{
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+			},
+		],
+	},
 }
