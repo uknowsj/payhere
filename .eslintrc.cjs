cd312bb605b21a70db4ee6000c0e835ef0b0d50d
Seungjoo Yang <uknowsj@gmail.com>
2024-05-05 15:36:43 +0900

    🔧 Lint Rule 및 스타일 config 변경 (#5)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
 		'plugin:prettier/recommended',
+		'plugin:unocss/recommended',
 		'plugin:tailwindcss/recommended',
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -14,6 +15,8 @@ module.exports = {
 	rules: {
 		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 		'@typescript-eslint/no-unused-vars': 'warn',
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'unocss/classnames-order': 'off',
 		'import/order': [
 			'warn',
 			{
