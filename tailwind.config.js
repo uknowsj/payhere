947bc57cbfa101a47cfe5ae0cb201dba847fbcca
uknowsj <uknowsj@gmail.com>
2024-05-05 15:35:38 +0900

    🔧 Lint Rule 및 스타일 config 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const pxToRem = (px, base = 16) => `${px / base}rem`
+
 export default {
+	important: true,
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#3F64ED',
+			},
+			spacing: {
+				...Array.from({ length: 300 }, (_, i) => i).reduce((acc, px) => {
+					acc[`${px}pxr`] = pxToRem(px)
+					return acc
+				}, {}),
+			},
+		},
 	},
 	plugins: [],
 }
