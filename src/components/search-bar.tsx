88b8fe3e9439bc37ac14c556e070de9235a464f1
uknowsj <uknowsj@gmail.com>
2024-05-05 15:44:16 +0900

    메인 페이지 컴포넌트 추가

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,12 +1,21 @@
+import { useContext } from 'react'
+
 import DatePicker from '@/components/date-picker'
+import DateContext from '@/context/date-context'
 import Button from '@/ui/button'
 
 export default function SearchBar() {
+	const { date, setDate } = useContext(DateContext)
+	const searchHoliday = () => {}
+
+	const changeStartDate = (date: Date) => setDate((prev) => ({ ...prev, start: date }))
+	const changeEndDate = (date: Date) => setDate((prev) => ({ ...prev, end: date }))
+
 	return (
 		<div className='flex h-89pxr items-center rounded-lg bg-white px-20pxr'>
-			<DatePicker label='시작일' className='flex-1' />
-			<DatePicker label='종료일' className='flex-1' />
-			<Button size='lg' className='w-128pxr'>
+			<DatePicker type='start' date={date.start} onChangeDate={changeStartDate} className='flex-1' />
+			<DatePicker type='end' date={date.end} onChangeDate={changeEndDate} className='flex-1' />
+			<Button size='lg' className='w-128pxr' onClick={searchHoliday}>
 				검색
 			</Button>
 		</div>
