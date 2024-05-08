ed0b115015d6d13152a5586f46f9fde25c2521b7
uknowsj <uknowsj@gmail.com>
2024-05-08 12:31:27 +0900

    🐛 공휴일 조회 오류 수정 - 연도 변경시에도 캘린더에 표시되게 변경

--- src/hooks/use-holiday.ts
+++ src/hooks/use-holiday.ts
@@ -3,12 +3,13 @@ import { useQuery } from '@tanstack/react-query'
 import { getHolidayInfo } from '@/api/holidays'
 import { ONE_DAY } from '@/constant/date'
 
-const useHoliday = (date: Date) => {
+const useHoliday = (year: number) => {
 	const { data } = useQuery({
-		queryKey: ['holiday', date.getFullYear()],
-		queryFn: () => getHolidayInfo(date.getFullYear()),
+		queryKey: ['holiday', year],
+		queryFn: () => getHolidayInfo(year),
 		staleTime: ONE_DAY,
 		gcTime: ONE_DAY,
+		throwOnError: true,
 	})
 
 	return { data }
